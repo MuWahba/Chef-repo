df3f0783622b8399ed89f5ea7bf94215b99eb962
wahba <mwe@wahbaco.com>
2017-09-30 05:58:45 -0700

    change apache attributes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,8 +3,16 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+if node['platform_family'] == "rhel" 
+	package = "httpd"
+elsif node['platform_family'] == "debian"
+	package = "apache2"
+end
+
+
 package 'apache' do 
-	package_name 'httpd'
+	package_name package
 	action :install
 end
 
@@ -13,7 +21,6 @@ service 'apache' do
 	action [:start, :enable]
 end
 
-include_recipe 'apache::website'
 
 
 
